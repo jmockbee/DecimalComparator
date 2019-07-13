f40fc251632170375520d1c036a914f0adc19378
Jon Mockbee <mockbee1000@gmail.com>
2019-07-13 10:31:38 -0400

    solving

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,4 +1,20 @@
 public class DecimalComparator {
 
+public static boolean
+    public static boolean areEqualByThreeDecimalPlaces(double number1, double number2){
+
+        int num1 = (int)(number1 * 1000);
+
+        int num2 = (int)(number2 * 1000);
+
+        if (num1 == num2){
+
+            return true;
+        }
+        else
+
+            return false;
+
+
 
 }
